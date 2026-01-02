76c1f3a262003c626f50771359270ba79897d99d
Agha Kazim Ali <akazim780@gmail.com>
2026-01-02 11:57:42 +0500

    Update Sales_perf.py

--- Sales_perf.py
+++ Sales_perf.py
@@ -5,12 +5,11 @@ import seaborn as sns
 
 df = pd.read_csv('sales_data_100k.csv')
 
-
 print("Shape:", df.shape)
 print("\nColumns:")
 print(df.columns)
 
-print("\nData Info:")
+print("\nInfo:")
 df.info()
 
 print("\nMissing values:")
@@ -19,13 +18,52 @@ print(df.isna().sum())
 print("\nDuplicate rows:")
 print(df.duplicated().sum())
 
-print("\nProfit summary:")
+### PHASE 1 COMPLETED ###
+
 df['profit'] = df['sales'] - df['cost']
+
+print("\nProfit stats:")
 print(df['profit'].describe())
 
-print("\nSample losses:")
+print("\nLoss :")
 print(df[df['profit'] < 0].head())
 
-print("\nZero profit rows:")
+print("\nZero-profit :")
 print(df[df['profit'] == 0].head())
+
+## PHASE 2 COMPLETED ##
+
+Top_profit = df.groupby('product')[['sales', 'profit']].sum()
+
+Top_profit['Profit_margin%'] = (
+    Top_profit['profit'] / Top_profit['sales']
+    ) * 100
+print(Top_profit)
+
+Top_profit = Top_profit.sort_values(
+    by='profit', ascending=False
+    )
+
+print(Top_profit)
+
+## PHASE 3 COMPLETED    
+
+region_summary = (
+    df.groupby("region")
+      .agg(
+          total_sales=("sales", "sum"),
+          total_profit=("profit", "sum")
+      )
+)
+
+region_summary['avg_profit_region'] = (
+    region_summary['total_profit'] / region_summary['total_sales'] 
+) * 100
+
+region_summary = region_summary.sort_values(
+    by='total_profit', ascending=False
+)
+
+print(region_summary)
+
 
